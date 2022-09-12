ebad58927730956af6be77a090258b63fa9d9552
EbbaSchmid <justebba@gmail.com>
2022-09-12 11:18:23 -0500

    quiz questions

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log ("hello")
+// 1) Define the required variables used to track the state of the game
+let powerToolQuestions
+let handToolQuestions
+let specialityToolQuestions
+let equipmentQuestions
+
+
+// 2) Store cached element references
+
+const powerToolQuestions = document.querySelector
+
+// 3) Upon loading, the game state should be initialized, and a function should be called to render this game state
+
+
+
+// 4) The state of the game should be rendered to the user
+
+
+
+// 5) Define the required constants
+let powerToolQuestions = {
+    qNumber: 1,
+    question: "What Power Tool is this?",
+    answer: "Saw Zall",
+    answerOptions: ["Saw", "Hammer Drill", "Hack Saw", "Saw Zall"]
+
+    qNumber: 2,
+    question: "What Power Tool is this?",
+    answer: "Hammer Drill",
+    answerOptions: ["Saw", "Hammer Drill", "Hack Saw", "Saw Zall"]
+    }
+
+
+// 6) Handle a player clicking a square with a `handleClick` function
+
+
+
+// 7) Build the `getWinner` function
+
+
+
+// 8) Create Reset functionality
